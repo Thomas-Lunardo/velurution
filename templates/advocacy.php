4499ec55626dae9d391f5286b2d5691795ce33e2
THOMAS <thomaslunardo@gmail.com>
2024-06-20 22:10:34 +0200

    duplicated the burge menu to all pages

--- templates/advocacy.php
+++ templates/advocacy.php
@@ -15,29 +15,41 @@ require '../src/arrays.php';
 
 <body>
   <header>
-    <a href="../index.php"><img src="../assets/icones/icon_house.png" alt="icone d'une maison" /></a>
     <h1>Plaidoyer</h1>
   </header>
   <nav>
-    <ul>
-      <li><a href="../index.php">Accueil</a></li>
-      <li><a href="../templates/sunday.php">Vélorution</a></li>
-      <li  class="active"><a href="./advocacy.php">Plaidoyer</a></li>
-    </ul>
-  </nav>
-  <main>
-    <p>Pour que les enfants puissent se déplacer en autonomie vers leurs activités, dans des quartiers plus vivants et respirables, nous demandons :
-    </p>
-    <section class="advocacy-grid">
-      <?php foreach ($advocacy as $key => $value) { ?>
-        <div>
-          <img src="<?php echo $value['star'] ?>" alt="<?php echo $value['alt'] ?>" class="<?php echo $value['class'] ?>" loading="lazy">
-          <h4><?php echo $value['title'] ?></h4>
-          <p><?php echo $value['argument'] ?></p>
-        </div>
-      <?php } ?>
-    </section>
-  </main>
+    <nav class="navbar-desktop">
+      <ul>
+        <li class="active"><a href="../index.php">Accueil</a></li>
+        <li><a href="./templates/sunday.php">Vélorution</a></li>
+        <li><a href="#">Plaidoyer</a></li>
+      </ul>
+    </nav>
+    <nav class="navbar-mobile">
+      <img src="../assets/icones/menu-burger.png" alt="icone d'un menu burger" class="burger">
+      <ul class="nav-list-mobile">
+        <li><a href="../index.php">Accueil</a></li>
+        <li><a href="#">Plaidoyer</a></li>
+        <li><a href="./sunday.php">Vélorution</a></li>
+        <li><a href="https://clavette-lyon.heureux-cyclage.org/index.php/2024/01/24/samedi-22-juin-2024-fete-des-ateliers-velo-participatifs/">Fête des ateliers vélo</a>
+          <p>Site La clavette</p>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <p>Pour que les enfants puissent se déplacer en autonomie vers leurs activités, dans des quartiers plus vivants et respirables, nous demandons :
+      </p>
+      <section class="advocacy-grid">
+        <?php foreach ($advocacy as $key => $value) { ?>
+          <div>
+            <img src="<?php echo $value['star'] ?>" alt="<?php echo $value['alt'] ?>" class="<?php echo $value['class'] ?>" loading="lazy">
+            <h4><?php echo $value['title'] ?></h4>
+            <p><?php echo $value['argument'] ?></p>
+          </div>
+        <?php } ?>
+      </section>
+    </main>
+    <script src="../assets/script.js"></script>
 </body>
 
 </html>
